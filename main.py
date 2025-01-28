71e40d9e4014a36e3f7cb5b1e2081c3ffa1738b6
iuly20-07 <iulian.moian@gmail.com>
2025-01-28 15:26:39 +0200

    removed useless function

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# This function currently serves no purpose. It is one of the first functions and it may still serve some purpose. If, in the end, it does not, then it should be removed.
-def count_characters(text):
-    characters = {}
-    text = text.lower()
-    for char in text:
-        if char not in characters:
-            characters[char] = 1
-        else:
-            characters[char] += 1
-    return characters
-
 def letter_report(text):
     characters = {}
     text = text.lower()
@@ -30,7 +19,6 @@ def main():
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{word_count} words found in the document")
     print()
-    character_count = count_characters(file_contents) #check line 1
     letter_report(file_contents)
     print("--- End report ---")
 main()
