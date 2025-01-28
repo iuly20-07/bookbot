baddf5ff093668b8915a396eeae0fc07d3af8c22
iuly20-07 <iulian.moian@gmail.com>
2025-01-28 14:25:46 +0200

    add library file; update main with character counter

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+def count_letters(text):
+    from library import characters
+    text = text.lower()
+    for char in text:
+        if char in characters:
+            characters[char] += 1
+    return characters
+        
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     words = file_contents.split()
-    print (len(words))
+    word_count = len(words)
+    print (count_letters(file_contents))
 
 main()
