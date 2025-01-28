d987c87255f6e08b1020f1f4585679afa9cad0d2
iuly20-07 <iulian.moian@gmail.com>
2025-01-28 15:24:20 +0200

    update main with better formatted and sorted report

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This function currently serves no purpose. It is one of the first functions and it may still serve some purpose. If, in the end, it does not, then it should be removed.
 def count_characters(text):
     characters = {}
     text = text.lower()
@@ -19,15 +20,17 @@ def letter_report(text):
                 characters[char] += 1
     characters = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
     for char, count in characters.items():
-        print(f"The {char} was found {count} times")
+        print(f"The '{char}' character was found {count} times")
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     words = file_contents.split()
     word_count = len(words)
-    print(f"{word_count} words are found in the document")
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
     print()
-    character_count = count_characters(file_contents)
+    character_count = count_characters(file_contents) #check line 1
     letter_report(file_contents)
+    print("--- End report ---")
 main()
