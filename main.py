af1251308f41a4b6d2311fc4b8ac619344c8b6a8
iuly20-07 <iulian.moian@gmail.com>
2025-01-28 15:02:45 +0200

    update main to print report of word count and sorted list of characters by count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_letters(text):
+def count_characters(text):
     characters = {}
     text = text.lower()
     for char in text:
@@ -7,13 +7,27 @@ def count_letters(text):
         else:
             characters[char] += 1
     return characters
-        
+
+def letter_report(text):
+    characters = {}
+    text = text.lower()
+    for char in text:
+        if char.isalpha():
+            if char not in characters:
+                characters[char] = 1
+            else:
+                characters[char] += 1
+    characters = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
+    for char, count in characters.items():
+        print(f"The {char} was found {count} times")
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     words = file_contents.split()
     word_count = len(words)
-    print (count_letters(file_contents))
-
+    print(f"{word_count} words are found in the document")
+    print()
+    character_count = count_characters(file_contents)
+    letter_report(file_contents)
 main()
